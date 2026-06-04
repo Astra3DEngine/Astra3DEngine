cf8f9169a15b720a4755a971ee9b4ee24c377846
Cyberexplorer <3091949883@qq.com>
2026-06-04 23:33:44 +0800

    ci : try new wrokflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: process.env.BASE_URL || '/',
   server: {
     port: 3000,
     open: true
